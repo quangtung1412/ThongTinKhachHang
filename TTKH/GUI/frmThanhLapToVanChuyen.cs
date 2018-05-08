0c57090d057038504042453f7b404977f8b5249f
quangtung1412 <quangtung1412@gmail.com>
2018-05-08 22:46:50 +0700

    Commit 08052018 4

--- TTKH/GUI/frmThanhLapToVanChuyen.cs
+++ TTKH/GUI/frmThanhLapToVanChuyen.cs
@@ -60,8 +60,11 @@ namespace AGRIBANKHD.GUI
             listNguon.Add(DateTime.Now.Year.ToString());
             //To van chuyen
 <<<<<<< HEAD
+<<<<<<< HEAD
             
 =======
+=======
+>>>>>>> parent of 9ad0dcc... Merge branch 'master' of https://github.com/quangtung1412/ThongTinKhachHang
             int index = 0;
             listDich.Add("<TO_TRUONG>");
             if (cbToTruong.SelectedItem != null)
@@ -114,6 +117,9 @@ namespace AGRIBANKHD.GUI
             listDich.Add("<BAO_VE>");
             listNguon.Add(index + ". " + "Ông/Bà: " + txtHoTenLaiXe.Text + ", Số hộ chiếu/CMND/CCCD: " + txtCMNDLaiXe.Text +
                 " Ngày cấp: " + txtNgayCapLaiXe.Text + ",Nơi cấp: " + txtNoiCapLaiXe.Text + ";Chức danh: Bảo vệ;");
+<<<<<<< HEAD
+>>>>>>> parent of 9ad0dcc... Merge branch 'master' of https://github.com/quangtung1412/ThongTinKhachHang
+=======
 >>>>>>> parent of 9ad0dcc... Merge branch 'master' of https://github.com/quangtung1412/ThongTinKhachHang
 
             listDich.Add("<HANG_DAC_BIET>");
@@ -157,6 +163,7 @@ namespace AGRIBANKHD.GUI
         {
             Microsoft.Office.Interop.Word.Application ap = new Microsoft.Office.Interop.Word.Application();
             Microsoft.Office.Interop.Word.Document document = ap.Documents.Open(fileLocation);
+<<<<<<< HEAD
 <<<<<<< HEAD
             ap.Visible = true;
         }
@@ -245,6 +252,22 @@ namespace AGRIBANKHD.GUI
             document.Save();
         }
 
+=======
+            //ap.Visible = false;
+            //try
+            //{
+            //    document.PrintOut();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Vui lòng kiểm tra máy in!", "Thông báo", MessageBoxButtons.OK);
+            //}
+            //document.Close();
+            //ap.Quit();
+            ap.Visible = true;
+        }
+
+>>>>>>> parent of 9ad0dcc... Merge branch 'master' of https://github.com/quangtung1412/ThongTinKhachHang
 =======
             //ap.Visible = false;
             //try
